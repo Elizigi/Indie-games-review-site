cfe6946e1016bf705be64947b2e5261a7217a802
Elizigi <elizigi876@gmail.com>
2025-04-17 18:12:47 +0300

    fetchById

--- server/src/routes/gamesRoutes.ts
+++ server/src/routes/gamesRoutes.ts
@@ -2,9 +2,11 @@ import express from "express";
 import addGame from "../controllers/gameControllers/addGame";
 import fetchGames from "../controllers/gameControllers/fetchGames";
 import authMiddleware from "../controllers/userControllers/userAuth";
+import fetchGameById from "./fetchGameById";
 const router = express.Router();
 
 router.post("/add-game", authMiddleware, addGame );
 router.get("/fetch-games",fetchGames);
+router.get("/fetch-game:id",fetchGameById);
 
 export default router;
